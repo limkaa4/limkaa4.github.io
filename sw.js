481ea4b504db2fda94876f985467a89b4ad31e7c
Дмитрий <dima@eqwile.com>
2024-02-07 15:41:48 +0300

    add logs

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
 importScripts('https://wp.24smi.org/js/smi-webpush-sw-dev.js');
+
+console.log('globals')
+
+self.addEventListener("install", event => {
+    console.log('install')
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    console.log('activate')
+
+    event.waitUntil(self.clients.claim());
+});
